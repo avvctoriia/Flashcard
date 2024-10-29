1f5abb626e131b014fc7b8ecf5c3540fd1a11b84
victoria <140830059+avvctoriia@users.noreply.github.com>
2024-10-29 08:24:04 -0300

    eg

---
+++ pergunta.js
@@ -0,0 +1,35 @@
+criaCartao(
+    "Sociologia",
+    "o que é relativismo cultural?",
+    "é um conceito perspectiva que categoriza as cultura como “melhores” ou “superior”"
+)
+criaCartao(
+    "Matemática",
+    "quanto é  5!?",
+   "120" 
+)
+criaCartao(
+    "Biologia",
+    "se o homem evoluiu do macaco, porque ainda existem macacos?",
+    "os macaco continuam existindo porque eles são resultado de evolução o seu ancestral comum com o homem<"
+)
+criaCartao(
+    "Língua Portuguesa",
+    "o que é um ditongo?",
+    "são palavras que pouem duas vogais juntas"
+)
+criaCartao(
+    "Matemática 2",
+    "o que é uma class no html?",
+    "são utilizadas para agrupar elementos com características semelhantes"
+)
+criaCartao(
+    "Inglês",
+    "qual a tradução da frase “ I’m going to the mall’’?",
+    "eu estou indo ao shopping  "
+)
+criaCartao(
+    "Biologia 1",
+    "como ficou conhecido o Gregor Mendel?",
+    "pai da génetica"
+)
